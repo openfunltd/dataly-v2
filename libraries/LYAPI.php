ac67c132657511d4239618bb462048e73098bd9f
Ronny Wang <ronnywang@gmail.com>
2024-09-05 16:39:46 +0800

    add LYAPI::apiQuery($url, $reason) to call api

--- libraries/LYAPI.php
+++ libraries/LYAPI.php
@@ -12,4 +12,26 @@ class LYAPI
     {
         return self::$log;
     }
+
+    public static function apiQuery($url, $reason)
+    {
+        if (getenv('LYAPI_HOST')) {
+            $url = 'https://' . getenv('LYAPI_HOST') . $url;
+        } else {
+            $url = 'https://v2.ly.govapi.tw' . $url;
+        }
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $res = curl_exec($curl);
+        $res_json = json_decode($res);
+        curl_close($curl);
+        if (is_null(self::$log)) {
+            self::$log = [];
+        }
+        self::$log[$reason] = [$url, $reason];
+
+        return $res_json;
+    }
 }
