1e04dccd2184105e9408e772a4fea5bec62e385c
typingmonk <tmonk.hsieh@tmonk.tw>
2024-09-11 21:52:45 +0800

    Add datatable in ivod_video #1

--- views/collection/ivod_video.php
+++ views/collection/ivod_video.php
@@ -1,6 +1,36 @@
 <?php
-    echo $this->escape($this->data->data->video_url) . "<br>";
+    //echo $this->escape($this->data->data->video_url) . "<br>";
+    var_dump($data);
 ?>
+<div id="ai-transcript" class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h1 class="h3 mb-0 text-gray-800">
+            <?= $this->escape($this->data->data->委員名稱) ?> @
+            <?= $this->escape($this->data->data->會議資料->標題) ?>
+        </h1>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-12 col-lg-6">
+                <div>
+                    <table id="subtitleTable" class="table table-hover table-sm">
+                        <thead>
+                            <tr>
+                                <th>Start Time</th>
+                                <th>End Time</th>
+                                <th>Text</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <video id="video" controls width="100%"></video>
+            </div>
+        </div>
+    </div>
 <div>
   <video id="video" controls width="100%"></video>
 </div>
@@ -9,13 +39,7 @@
     if(Hls.isSupported()) {
         var video = document.getElementById('video');
         var hls = new Hls();
-
-        // working
-        hls.loadSource('https://h264media01.ly.gov.tw:443/vod_1/_definst_/mp4:1M/cbbd4ed0b5fcd3a07e9323a110d342f509b1d301fcc61009ab4dc72a1a8924cd5905afd99abfec235ea18f28b6918d91.mp4/playlist.m3u8');
-
-        // not work
-        //hls.loadSource(<?= $this->escape($this->data->data->video_url) ?>);
-
+        hls.loadSource(<?= json_encode($this->escape($this->data->data->video_url)) ?>);
         hls.attachMedia(video);
         hls.on(Hls.Events.MANIFEST_PARSED,function() {
             video.play();
