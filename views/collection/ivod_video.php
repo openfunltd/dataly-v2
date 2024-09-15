1a7c8122899dcf7e4d23dff7649a46e53ed535d0
typingmonk <tmonk.hsieh@tmonk.tw>
2024-09-15 08:51:13 +0800

    Add ai-transcript text and timestamp on datatable #1

--- views/collection/ivod_video.php
+++ views/collection/ivod_video.php
@@ -1,12 +1,35 @@
 <?php
-    //echo $this->escape($this->data->data->video_url) . "<br>";
-    var_dump($data);
+    if (!in_array('ai-transcript', $this->data->data->支援功能)) {
+        echo '無 AI 逐字稿<br>';
+        return;
+    }
+    $MP_name = $this->escape($this->data->data->委員名稱);
+    $meet_title = $this->escape($this->data->data->會議資料->標題);
+    $ai_transcript = [];
+    foreach ($this->data->data->transcript->whisperx as $idx => $segment) {
+        $content = new stdClass();
+        $start = sprintf(
+            "%02d:%02d:%02d,%03d",
+            $segment->start / 3600,
+            $segment->start / 60 % 60,
+            $segment->start % 60, (1000 * $segment->start) % 1000
+        );
+        $end = sprintf(
+            "%02d:%02d:%02d,%03d",
+            $segment->end / 3600,
+            $segment->end / 60 % 60,
+            $segment->end % 60, (1000 * $segment->end) % 1000
+        );
+        $content->start = $start;
+        $content->end = $end;
+        $content->text = $this->escape($segment->text);
+        $ai_transcript[] = $content;
+    }
 ?>
 <div id="ai-transcript" class="card shadow mb-4">
     <div class="card-header py-3">
         <h1 class="h3 mb-0 text-gray-800">
-            <?= $this->escape($this->data->data->委員名稱) ?> @
-            <?= $this->escape($this->data->data->會議資料->標題) ?>
+            <?= $MP_name ?> @ <?= $meet_title ?>
         </h1>
     </div>
     <div class="card-body">
@@ -22,6 +45,13 @@
                             </tr>
                         </thead>
                         <tbody>
+                            <?php foreach ($ai_transcript as $idx => $segment): ?>
+                                <tr id="s-{{ $idx }}">
+                                    <td><?= $segment->start ?></td>
+                                    <td><?= $segment->end ?></td>
+                                    <td><?= $segment->text ?></td>
+                                </tr>
+                            <?php endforeach; ?>
                         </tbody>
                     </table>
                 </div>
