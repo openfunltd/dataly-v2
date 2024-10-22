8794bb761c9a5d6828f4080b1cd25880803f3968
typingmonk <tmonk.hsieh@tmonk.tw>
2024-10-22 16:42:47 +0800

    Update new data arrangement of related_document(關係文書) #3

--- views/collection/bill_related-document.php
+++ views/collection/bill_related-document.php
@@ -1,91 +1,82 @@
 <?php
     $bill = $this->data->data;
-
+    $attaches = $bill->相關附件 ?? [];
+    $has_related_doc = false;
+    foreach ($attaches as $attach) {
+        if (mb_strpos($attach->名稱, '關係文書') !== false) {
+            $has_related_doc = true;
+            break;
+        }
+    }
+?>
+<?php if (!$has_related_doc) { ?>
+  <div class="mt-2 card border-left-danger">
+    <div class="card-body">
+      無關係文書
+    </div>
+  </div>
+<?php return; ?>
+<?php } ?>
+<?php
     //retrieve data
     $bill_source = $bill->提案來源 ?? '';
     $bill_source_title = $bill->{'提案單位/提案委員'} ?? '';
-    $proposers = $bill->提案人 ?? [];
+    $proposers = implode('、', $bill->提案人 ?? []);
     $endorsers = implode('、', $bill->連署人 ?? []);
-
-    //提案人
-    if ($bill_source != '委員提案' or empty($proposers)) {
-        $proposers = $bill_source_title;
-    } else {
-        $proposers = implode('、', $proposers);
-    }
 
     //對照表
     $diff = null;
     if (property_exists($bill, '對照表')) {
+        $law_diff_title = $bill->對照表[0]->title ?? '法律對照表';
         $diff = LawDiffHelper::lawDiff($bill);
     }
 ?>
 <link href="/static/css/bill/custom_law-diff.css" rel="stylesheet">
 <div class="card shadow mt-3 mb-3">
-  <a href="#collapseMetadata" class="d-block card-header py-3" data-toggle="collapse"
-    role="button" aria-expanded="true" aria-controls="collapseMetadata">
-    <h6 class="m-0 font-weight-bold text-primary">基本資料</h6>
-  </a>
-  <div class="collapse show" id="collapseMetadata">
-    <div class="card-body">
-      <table class="table">
+  <div class="card-body">
+    <table class="table">
+      <?php if (property_exists($bill, '字號')) { ?>
+      <tr>
+        <td style="width: 15%">字號</td>
+        <td><?= $this->escape($bill->字號) . "（" . $this->escape($bill->提案編號) . "）" ?></td>
+      </tr>
+      <?php } ?>
+      <?php if (property_exists($bill, '案由')) { ?>
         <tr>
-          <td style="width: 10%;">提案編號</td>
-          <td><?= $this->escape($this->data->id[0]) ?></td>
+          <td style="width: 15%">案由</td>
+          <td><?= $this->escape($bill->案由) ?></td>
         </tr>
-        <?php if (property_exists($bill, '字號')) { ?>
+      <?php } ?>
+      <?php if ($bill_source != '委員提案') { ?>
+      <tr>
+        <td style="width: 15%">提案單位</td>
+        <td><?= $this->escape($bill_source_title) ?></td>
+      </tr>
+      <?php } ?>
+      <?php if ($bill_source == '委員提案') { ?>
+      <tr>
+        <td style="width: 15%">提案人</td>
+        <td><?= $this->escape($proposers) ?></td>
+      </tr>
         <tr>
-          <td>字號</td>
-          <td><?= $this->escape($bill->字號) . "（" . $this->escape($bill->提案編號) . "）" ?></td>
+          <td>連署人</td>
+          <td><?= $this->escape($endorsers) ?></td>
         </tr>
-        <?php } ?>
-        <?php if (property_exists($bill, '會議代碼:str')) { ?>
-        <tr>
-          <td>關聯會議</td>
-          <td>
-            <?= $this->escape($bill->{'會議代碼:str'}) ?>
-            （<a href="/collection/item/meet/<?= $this->escape($bill->會議代碼) ?>">連結</a>）
-          </td>
-        </tr>
-        <?php } ?>
-        <?php if (!empty($proposers)) { ?>
-        <tr>
-          <td>提案人</td>
-          <td><?= $this->escape($proposers) ?></td>
-        </tr>
-        <?php } ?>
-        <?php if (!empty($endorsers)) { ?>
-          <tr>
-            <td>連署人</td>
-            <td><?= $this->escape($endorsers) ?></td>
-          </tr>
-        <?php } ?>
-        <?php if (property_exists($bill, '案由')) { ?>
-          <tr>
-            <td>案由</td>
-            <td><?= $this->escape($bill->案由) ?></td>
-          </tr>
-        <?php } ?>
-        <?php if (property_exists($bill, '相關附件') and count($bill->相關附件) > 0) { ?>
-          <?php foreach ($bill->相關附件 as $idx => $attach) {
-            $attach_url = (mb_strpos($attach->網址, 'https') === 0) ? $attach->網址 : 'https://ppg.ly.gov.tw' . $attach->網址;
-            $attaches[] = sprintf('<a href="%s" target="_blank">%s</a>', $this->escape($attach_url), $this->escape($attach->名稱));
-          } ?>
-          <tr>
-            <td>相關附件</td>
-            <td>
-              <?= implode("<br>", $attaches) ?>
-            </td>
-          </tr>
-        <?php } ?>
-        <tr>
-          <td>原始資料</td>
-          <td>
-            <a href="<?= $this->escape($bill->url) ?>" target="blank">議事暨公報資訊網</a>
-          </td>
-        </tr>
-      </table>
-    </div>
+      <?php } ?>
+      <?php foreach ($attaches as $attach) {
+          if (mb_strpos($attach->名稱, '關係文書') === false) {
+              continue;
+          }
+          $attach_url = (mb_strpos($attach->網址, 'https') === 0) ? $attach->網址 : 'https://ppg.ly.gov.tw' . $attach->網址;
+          $links[] = sprintf('<a href="%s" target="_blank">%s</a>', $this->escape($attach_url), $this->escape($attach->名稱));
+      } ?>
+      <tr>
+        <td style="width: 15%">檔案連結</td>
+        <td>
+          <?= implode("<br>", $links) ?>
+        </td>
+      </tr>
+    </table>
   </div>
 </div>
 <?php if (is_null($diff)) { ?>
@@ -96,6 +87,7 @@
 </div>
 <?php } ?>
 <?php if (isset($diff)) { ?>
+<h2 class="ml-2 mt-4 mb-3 h5"><?= $law_diff_title ?></h2>
 <div class="row">
   <div class="col-lg-2 law-idx-list">
     <div class="card shadow mb-4">
